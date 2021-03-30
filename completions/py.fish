e792bce1b4f8e68991daed7681d44a5aa0527eb8
Brett Cannon <brett@python.org>
2021-03-29 21:22:23 -0700

    Do full completions for fish Closes #84

--- completions/py.fish
+++ completions/py.fish
@@ -9,5 +9,19 @@ complete -c py --long-option list --no-files -d "List all known interpreters"
 complete -c py --short-option h --long-option help --no-files -d "Display help and exit"
 
 # Dynamic/system-specific completions.
-# XXX Major-minor
-# XXX Major-only
+set -l seen_major_versions
+py --list | while read -d " │ " -l padded_version padded_path
+    # Complete on the `major.minor` version.
+    set -l full_version (string trim $padded_version)
+    set -l executable_path (string trim $padded_path)
+    complete -c py --old-option $full_version -d "Launch $executable_path"
+    # Complete on the major version.
+    # Assume that `py --list` emits a sorted list of versions, so the
+    # first instance of any major version is the one that will be used.
+    set -l major_version (string split --fields 1 . $full_version)
+    if not contains $major_version $seen_major_versions
+        # Must use `--old-option` in case the major version ever goes multi-digit.
+        complete -c py --old-option $major_version -d "Launch $executable_path"
+        set --append seen_major_versions $major_version
+    end
+end
